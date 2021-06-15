a18aa2a24435ff352eecd7a246b9c761bec15d69
Zsolt Ban <banzsolt94@gmail.com>
2021-06-16 00:34:16 +0300

    Initial commit

---
+++ SpeedometerParameter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NinjaTrader.Custom
+{
+    public class SpeedometerParameter
+    {
+        //Speedo Settings
+        public string Name { get; set; }
+        public float MinValue { get; set; }
+        public float MaxValue { get; set; }
+
+        //Speedo Value
+        public float Value { get; set; }
+    }
+}
---
+++ WebsocketHelper.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Newtonsoft.Json.Linq;
+using WebSocketSharp.Server;
+
+namespace NinjaTrader.Custom
+{
+    class WebsocketHelper
+    {
+        public static event EventHandler<SpeedometerParameter> NewMessage;
+        private static bool wasInitialized = false;
+
+        private static void Init()
+        {
+            var wsEndpoint = "ws://127.0.0.1:1111";
+
+            var server = new WebSocketServer(int.Parse(wsEndpoint.Split(':').Last()));
+            server.AddWebSocketService<SpeedoWebService>("/WService");
+            server.Start();
+        }
+
+        public static void SendMessage(SpeedometerParameter value)
+        {
+            if (!wasInitialized)
+            {
+                Init();
+            }
+
+            if (NewMessage != null)
+            {
+                NewMessage.Invoke(value, value);
+            }
+        }
+
+    }
+
+
+    public class SpeedoWebService : WebSocketBehavior
+    {
+        private static bool alreadyConnected = false;
+
+        public SpeedoWebService()
+        {
+            if (!alreadyConnected)
+            {
+                alreadyConnected = true;
+                WebsocketHelper.NewMessage += NewMessageReceived;
+            }
+        }
+
+        protected override void OnOpen()
+        {
+
+        }
+
+        private void NewMessageReceived(object sender, SpeedometerParameter e)
+        {
+            try
+            {
+                if (this.State == WebSocketSharp.WebSocketState.Open)
+                {                    
+                    this.Send(JObject.FromObject(e).ToString());
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
